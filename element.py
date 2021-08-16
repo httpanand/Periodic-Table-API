c36132c0fda1fc7d5be88507faad7b381b01e243
Anand <87514488+httpanand@users.noreply.github.com>
2021-08-16 23:07:24 +0530

    Add files via upload

---
+++ element.py
@@ -0,0 +1,15 @@
+def h(): #hydrogen 
+       print("Element : Hydrogen")
+       print("Atomic Number : 1")
+
+def he(): #helium
+       print("Element : Heleium")
+       print("Atomic Number : 2")
+
+def li(): #lithium
+       print("Element : Lithium")
+       print("Atomic Number : 3")
+       
+def be(): #lithium
+       print("Element : Beryllium")
+       print("Atomic Number : 4")
---
+++ main.py
@@ -0,0 +1,16 @@
+from element import *
+input = input('Input chemical symbol [Case Sensitive] ')#input from user
+
+
+     
+if input == 'H':
+    print(h())
+elif input == 'He':
+    print(he())
+elif input == 'Li':
+    print(li())
+elif input == 'Be':
+    print(be())
+
+
+
