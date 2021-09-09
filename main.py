3dbdb54391b6a836f86ca5471e893849dcae9219
Anand <87514488+httpanand@users.noreply.github.com>
2021-09-09 11:53:58 +0000

    API

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
-from element import *
-input = input('Input chemical symbol [Case Sensitive] ')
+from flask import Flask, request, jsonify
+from flask import render_template
+
+app = Flask(__name__)
 
 
-     
-if input == 'H':
-    print(h())
-elif input == 'He':
-    print(he())
-elif input == 'Li':
-    print(li())
-elif input == 'Be':
-    print(be())
-elif input == 'B':
-    print(b())
-elif input == 'C':
-    print(c())
+@app.route('/')
+def html():
+    return render_template('main.html')
+    
 
+element = [
+  { 
+    'Name':'Hydrogen',
+    'Atomic Number':'1'
+  },    
+  {
+    'Name':'Heleium',
+    'Atomic Number':'2'
+  },
+  {
+    'Name':'Lithium',
+    'Atomic Number':'3'
+  },
+  {
+    'Name':'Beryllium',
+    'Atomic Number':'4'
+  }, 
+  {
+    'Name':'Boron',
+    'Atomic Number':'5'
+  },
+  {
+    'Name':'Carbon',
+    'Atomic Number':'6'
+  },
+  {
+    'Name':'Beryllium',
+    'Atomic Number':'7'
+  },
+]
 
+@app.route('/api', methods=['GET'])
+def api_all():
+    return jsonify(element)
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=8080)
